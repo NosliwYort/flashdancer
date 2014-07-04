2ad81390bc65566777cc0cc0f4edef8202929770
IdealEthos <idealethos@gmail.com>
2014-07-04 17:14:24 -0400

    Added associations to User, Cart, Receipt, and Product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  belongs_to :cart
+  belongs_to :receipt
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_one :cart
+  has_many :receipts
+  has_many :products, through: :receipts
 end
