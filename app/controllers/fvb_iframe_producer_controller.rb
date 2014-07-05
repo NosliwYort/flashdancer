d894ea8ddf2f15da139381b9238590bce3257afd
IdealEthos <idealethos@gmail.com>
2014-07-05 16:15:07 -0400

    Changed X-Frame-Options in order to enable the app to load in an iframe

--- app/controllers/fvb_iframe_producer_controller.rb
+++ app/controllers/fvb_iframe_producer_controller.rb
@@ -1,8 +1,14 @@
 class FvbIframeProducerController < ApplicationController
-
+  before_action :allow_iframe
   layout 'fvb_iframe_producer'
 
   def index
   end
+
+  private
+  
+    def allow_iframe
+	  response.headers.except! 'X-Frame-Options'
+  	end
 
 end
