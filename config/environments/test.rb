25227709ceb161531124d524fd47615ee7573392
IdealEthos <idealethos@gmail.com>
2014-07-05 12:00:22 -0400

    Updated Gemfile w/ Nokogiri, HTTParty, Bootstrap, Etc,

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ Flashdancer::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.action_controller.default_url_options = {:host => 'localhost'}
 end
